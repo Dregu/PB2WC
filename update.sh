4ab1823956258d56074303a0a0c7cc074813f342
Dregu <dregu@dreg.us>
2021-03-23 06:58:18 +0200

    move to subdirs

--- update.sh
+++ update.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+mkdir -p layouts
+mkdir -p images
 wget https://dfp529wcvahka.cloudfront.net/manifests/weeklyChallenges.json -O weeklyChallenges.json
 cat weeklyChallenges.json | jq -r '.[]|[.week,.payload,.preview]|@tsv' | while IFS=$'\t' read -r w p i; do
-  f=$(printf "%04d" $w).layout
+  f=layouts/$(printf "%04d" $w).layout
   if [ ! -f $f ]; then
     wget $p -O- | gunzip -d - > $f
   fi
-  f=$(printf "%04d" $w).png
+  f=images/$(printf "%04d" $w).png
   if [ ! -f $f ]; then
     wget $i -O $f
   fi
