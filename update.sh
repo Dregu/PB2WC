803740ac2d9c4e80228175c1ac932398f441df3e
Dregu <dregu@dreg.us>
2021-03-23 07:00:06 +0200

    tweaks

--- update.sh
+++ update.sh
@@ -3,11 +3,11 @@ mkdir -p layouts
 mkdir -p images
 wget https://dfp529wcvahka.cloudfront.net/manifests/weeklyChallenges.json -O weeklyChallenges.json
 cat weeklyChallenges.json | jq -r '.[]|[.week,.payload,.preview]|@tsv' | while IFS=$'\t' read -r w p i; do
-  f=layouts/$(printf "%04d" $w).layout
+  f=layouts/w$(printf "%04d" $w).layout
   if [ ! -f $f ]; then
     wget $p -O- | gunzip -d - > $f
   fi
-  f=images/$(printf "%04d" $w).png
+  f=images/w$(printf "%04d" $w).png
   if [ ! -f $f ]; then
     wget $i -O $f
   fi
